231fc8d9d0cc126d26c08ed28dc3803ae529bdc8
Julien de Lambilly <julien.delambilly@gmail.com>
2023-10-26 02:50:45 +0200

    Added OpenAI API integration to QA_chatbot.py, removed database file deletion in retrieval.py, and implemented various retrieval methods. Also added scikit-learn to requirements.txt.

---
+++ QA_chatbot.py
@@ -0,0 +1,32 @@
+import os
+import openai
+import sys
+sys.path.append('../..')
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+import datetime
+current_date = datetime.datetime.now().date()
+if current_date < datetime.date(2023, 9, 2):
+    llm_name = "gpt-3.5-turbo-0301"
+else:
+    llm_name = "gpt-3.5-turbo"
+print(llm_name)
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+persist_directory = 'docs/chroma/'
+embedding = OpenAIEmbeddings()
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+
+print(vectordb._collection.count())
+
+question = "What are major topics for this class?"
+docs = vectordb.similarity_search(question,k=3)
+len(docs)
+
+from langchain.chat_models import ChatOpenAI
+llm = ChatOpenAI(model_name=llm_name, temperature=0)
--- retrieval.py
+++ retrieval.py
@@ -14,13 +14,6 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 persist_directory = 'docs/chroma/'
 
-# Erase old database file in docs/chroma/ if any
-import shutil
-try:
-    shutil.rmtree(persist_directory)
-except FileNotFoundError:
-    pass
-
 embedding = OpenAIEmbeddings()
 vectordb = Chroma(
     persist_directory=persist_directory,
@@ -45,5 +38,101 @@ print(f'Question: "{question}"')
 print("Similarity search results:")
 print(smalldb.similarity_search(question, k=2))
 print()
+
+# Addressing Diversity: Maximum marginal relevance
 print("Max marginal relevance search results:")
 print(smalldb.max_marginal_relevance_search(question,k=2, fetch_k=3))
+
+
+# Addressing Specificity: working with metadata using self-query retriever
+
+from langchain.llms import OpenAI
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+
+metadata_field_info = [
+    AttributeInfo(
+        name="source",
+        description="The lecture the chunk is from, should be one of `docs/Assura-Basis_CGA_LAMal_2024_F.pdf`, `docs/NEW_ASSURA_CSC-Natura_2015.07_F.pdf`, or `docs/Vue_Ensemble_Produits_F_V33_08.2023.pdf`",
+        type="string",
+    ),
+    AttributeInfo(
+        name="page",
+        description="The page from the lecture",
+        type="integer",
+    ),
+]
+
+document_content_description = "Lecture notes"
+llm = OpenAI(temperature=0)
+retriever = SelfQueryRetriever.from_llm(
+    llm,
+    vectordb,
+    document_content_description,
+    metadata_field_info,
+    verbose=True
+)
+
+question = "Qu'en est-il de la souscription ?"
+
+docs = retriever.get_relevant_documents(question)
+
+for d in docs:
+    print(d['metadata'])
+
+
+# Additional tricks: compression
+
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+
+def pretty_print_docs(docs):
+    print(f"\n{'-' * 100}\n".join([f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]))
+
+
+# Wrap our vectorstore
+llm = OpenAI(temperature=0)
+compressor = LLMChainExtractor.from_llm(llm)
+
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor,
+    base_retriever=vectordb.as_retriever(search_type = "mmr") # use mmr to avoid duplicates
+)
+
+question = "qu'en est-il à propos des frais administratifs?"
+print(f'Question: "{question}"')
+compressed_docs = compression_retriever.get_relevant_documents(question)
+pretty_print_docs(compressed_docs)
+
+
+# Other types of retrieval
+
+from langchain.retrievers import SVMRetriever
+from langchain.retrievers import TFIDFRetriever
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Load PDF
+loader = PyPDFLoader("docs/Vue_Ensemble_Produits_F_V33_08.2023.pdf")
+pages = loader.load()
+all_page_text=[p.page_content for p in pages]
+joined_page_text=" ".join(all_page_text)
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1500,chunk_overlap = 150)
+splits = text_splitter.split_text(joined_page_text)
+
+# Retrieve
+svm_retriever = SVMRetriever.from_texts(splits,embedding)
+tfidf_retriever = TFIDFRetriever.from_texts(splits)
+
+question = "qu'en est-il à propos des frais administratifs?"
+docs_svm=svm_retriever.get_relevant_documents(question)
+print("SWMRetriver results:")
+print(docs_svm[0])
+
+print("- - -")
+question = "Qu'en est-il de la souscription ?"
+docs_tfidf=tfidf_retriever.get_relevant_documents(question)
+print("TFIDFRetriever results:")
+print(docs_tfidf[0])
