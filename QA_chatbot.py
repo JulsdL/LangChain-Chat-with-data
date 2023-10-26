91dc44237da0d9373c37d3374c07e8ed7a379ee0
Julien de Lambilly <julien.delambilly@gmail.com>
2023-10-26 04:10:07 +0200

    Enhanced QA_chatbot.py with RetrievalQA chain, prompt templates, and additional queries; added pytest configuration to .vscode/settings.json

--- QA_chatbot.py
+++ QA_chatbot.py
@@ -24,9 +24,63 @@ vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedd
 
 print(vectordb._collection.count())
 
-question = "What are major topics for this class?"
+question = "What are the major topics?"
+print(question)
 docs = vectordb.similarity_search(question,k=3)
 len(docs)
 
 from langchain.chat_models import ChatOpenAI
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
+
+# RetrievalQA chain
+
+from langchain.chains import RetrievalQA
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever()
+)
+
+result = qa_chain({"query": question})
+
+print(result["result"])
+
+# Prompt
+
+from langchain.prompts import PromptTemplate
+
+# Build prompt
+template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
+{context}
+Question: {question}
+Helpful Answer:"""
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+
+# Run chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
+)
+print("- - -")
+question = "Est-ce que la franshise est un sujet important?"
+print(question)
+
+result = qa_chain({"query": question})
+
+print(result["result"])
+print(result["source_documents"][0])
+
+# RetrievalQA chain types
+
+qa_chain_mr = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    chain_type="map_reduce"
+)
+
+result = qa_chain_mr({"query": question})
+print("RetrievalQA with map_reduce results:")
+print(result["result"])
